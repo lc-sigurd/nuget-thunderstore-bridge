0f742777970e46ad35d59b8e0c667075cb8ccc62
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-04 16:33:39 +0000

    add json converter for `NuGetFramework`

---
+++ build/schema/NuGetFrameworkJsonConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NuGet.Frameworks;
+using JsonException = Newtonsoft.Json.JsonException;
+
+namespace Build.Schema;
+
+public class NuGetFrameworkJsonConverter : JsonConverter<NuGetFramework>
+{
+    public override NuGetFramework? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType is not JsonTokenType.String)
+            throw new JsonException($"Found token {reader.TokenType} but expected token {JsonTokenType.String}");
+
+        using var doc = JsonDocument.ParseValue(ref reader);
+        return NuGetFramework.Parse(doc.RootElement.GetString()!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, NuGetFramework value, JsonSerializerOptions options)
+    {
+        var s = value.GetShortFolderName();
+        writer.WriteStringValue(s);
+    }
+}
