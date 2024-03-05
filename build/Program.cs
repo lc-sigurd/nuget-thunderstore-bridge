3638212930a61dd2052292a5a2d356288cbe83f4
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-05 08:02:51 +0000

    unpack license and readme from packages

--- build/Program.cs
+++ build/Program.cs
@@ -17,6 +17,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Build.Schema.Converters;
 using Build.Schema.Local;
@@ -32,7 +33,9 @@ using Json.Schema.Serialization;
 using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Frameworks;
+using NuGet.Packaging;
 using NuGet.Packaging.Core;
+using NuGet.Packaging.Licenses;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
@@ -90,6 +93,8 @@ public class BuildContext : FrostingContext
         get => _nuGetPackageDownloadResults ?? throw new InvalidOperationException();
         set => _nuGetPackageDownloadResults = new ReadOnlyDictionary<PackageIdentity, DownloadResourceResult>(value);
     }
+
+    public PackageReaderBase GetPackageReader(PackageIdentity identity) => NuGetPackageDownloadResults[identity].PackageReader;
 
     private ReadOnlyDictionary<PackageIdentity, IList<string>>? _runtimeItemPaths;
 
@@ -413,30 +418,198 @@ public sealed class DownloadNuGetPackagesTask : NuGetTaskBase
     }
 }
 
-[TaskName("Resolve runtime assemblies")]
+[TaskName("Copy runtime assemblies")]
 [IsDependentOn(typeof(PrepareTask))]
 [IsDependentOn(typeof(DownloadNuGetPackagesTask))]
 public sealed class CopyRuntimeAssembliesTask : AsyncFrostingTask<BuildContext>
 {
+    private static readonly HttpClient Client = new();
+    private static readonly Regex GitHubRichFileViewerUrl = new("(https?://github\\.com/.*)/(?:blob|tree)/(.*)", RegexOptions.Compiled);
+
     public override bool ShouldRun(BuildContext context)
     {
         if (context.PackageVersionsToBridge.Count == 0) return false;
         return base.ShouldRun(context);
     }
 
-    private async Task<IEnumerable<string>> FindItemsFor(BuildContext context, PackageIdentity identity)
+    private DirectoryPath GetPackageDestination(BuildContext context, PackageIdentity identity)
     {
-        var packageReader = context.NuGetPackageDownloadResults[identity].PackageReader;
-        var libItemsGroupPerFramework = await packageReader.GetLibItemsAsync(default);
-        if (libItemsGroupPerFramework is null) return Array.Empty<string>();
+        return context.IntermediateOutputDirectory.Combine($"package-lib/{identity.Id}/{identity.Id}-{identity.Version}");
+    }
+
+    private async Task<IEnumerable<string>> ExtractPackageItems(BuildContext context, PackageIdentity identity, string[] items)
+    {
+        var destination = GetPackageDestination(context, identity);
+        context.EnsureDirectoryExists(destination);
+
+        var packageFileExtractor = new PackageFileExtractor(items, XmlDocFileSaveMode.Skip);
+
+        return await context
+            .GetPackageReader(identity)
+            .CopyFilesAsync(
+                destination.FullPath,
+                items,
+                packageFileExtractor.ExtractPackageFile,
+                NullLogger.Instance,
+                default
+            );
+    }
+
+    private async Task<IEnumerable<string>> FindLibItems(BuildContext context, PackageIdentity identity)
+    {
+        var libItemsGroupPerFramework = await context
+            .GetPackageReader(identity)
+            .GetLibItemsAsync(default);
+        if (libItemsGroupPerFramework is null) return Enumerable.Empty<string>();
         var libItemsGroupForNearestFramework = NuGetFrameworkUtility.GetNearest(libItemsGroupPerFramework, context.CommunityConfiguration.RuntimeFramework, group => group.TargetFramework)!;
-        return libItemsGroupForNearestFramework.Items;
+        return libItemsGroupForNearestFramework.Items
+            .Where(item => item.EndsWith(".dll"));
+    }
+
+    private async Task FetchUri(BuildContext context, Uri uri, FilePath destination)
+    {
+        var response = await Client.GetAsync(uri);
+        if (response is not { IsSuccessStatusCode: true }) throw new Exception($"Failed to fetch {uri}");
+
+        var responseContent = await response.Content.ReadAsStreamAsync();
+        await using FileStream stream = File.OpenWrite(destination.FullPath);
+        await responseContent.CopyToAsync(stream);
+    }
+
+    private async Task FetchLicense(BuildContext context, NuGetLicense license, FilePath destination)
+    {
+        await FetchUri(context, new Uri($"https://github.com/spdx/license-list-data/raw/main/text/{license.Identifier}.txt"), destination);
+    }
+
+    private async Task<IEnumerable<string>> FetchLicensesInExpression(BuildContext context, NuGetLicenseExpression expression, DirectoryPath destination)
+    {
+        if (expression is WithOperator { Type: LicenseExpressionType.Operator } withOperator) {
+            return await FetchLicensesInExpression(context, withOperator.License, destination);
+        }
+
+        if (expression is LogicalOperator { Type: LicenseExpressionType.Operator } logicalOperator) {
+            var itemGroups = await Task.WhenAll(
+                FetchLicensesInExpression(context, logicalOperator.Left, destination),
+                FetchLicensesInExpression(context, logicalOperator.Right, destination)
+            );
+            return itemGroups.SelectMany(x => x);
+        }
+
+        if (expression is NuGetLicense { Type: LicenseExpressionType.License } license) {
+            var licensePath = destination.CombineWithFilePath($"{license.Identifier}.txt");
+            await FetchLicense(context, license, licensePath);
+            return [licensePath.FullPath];
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(expression.Type), $"Unrecognised {nameof(LicenseExpressionType)}");
+    }
+
+    private async Task<IEnumerable<string>> CopyLicense(BuildContext context, PackageIdentity identity)
+    {
+        var reader = context.GetPackageReader(identity);
+        var licenseMetadata = reader.NuspecReader.GetLicenseMetadata();
+        var licenseUrl = reader.NuspecReader.GetLicenseUrl();
+
+        if (licenseMetadata is { Type: LicenseType.Expression, LicenseExpression.Type: LicenseExpressionType.Operator }) {
+            var packageDestination = GetPackageDestination(context, identity);
+            var licenseItems = await FetchLicensesInExpression(context, licenseMetadata.LicenseExpression, packageDestination.Combine("licenses"));
+            var mainLicenseFilePath = packageDestination.CombineWithFilePath("LICENSE");
+            await using FileStream stream = File.OpenWrite(mainLicenseFilePath.FullPath);
+            await using StreamWriter writer = new StreamWriter(stream);
+            await writer.WriteAsync(licenseMetadata.LicenseExpression.ToString());
+            return licenseItems.Prepend(mainLicenseFilePath.FullPath);
+        }
+
+        if (licenseMetadata is { Type: LicenseType.Expression, LicenseExpression.Type: LicenseExpressionType.License }) {
+            var packageDestination = GetPackageDestination(context, identity);
+            var mainLicenseFilePath = packageDestination.CombineWithFilePath("LICENSE");
+            await FetchLicense(context, (NuGetLicense)licenseMetadata.LicenseExpression, mainLicenseFilePath);
+            return [mainLicenseFilePath.FullPath];
+        }
+
+        if (licenseMetadata is { Type: LicenseType.File }) {
+            return await ExtractPackageItems(context, identity, [licenseMetadata.License]);
+        }
+
+        if (!String.IsNullOrWhiteSpace(licenseUrl)) {
+            var packageDestination = GetPackageDestination(context, identity);
+            var mainLicenseFilePath = packageDestination.CombineWithFilePath("LICENSE");
+
+            var maybeMatch = GitHubRichFileViewerUrl.Match(licenseUrl);
+            if (maybeMatch is { Success: true }) {
+                var uri = new Uri($"{maybeMatch.Groups[1]}/raw/{maybeMatch.Groups[2]}");
+                await FetchUri(context, uri, mainLicenseFilePath);
+                return [mainLicenseFilePath.FullPath];
+            }
+
+            await FetchUri(context, new Uri(licenseUrl), mainLicenseFilePath);
+            return [mainLicenseFilePath.FullPath];
+        }
+
+        return Enumerable.Empty<string>();
+    }
+
+    private async Task<IEnumerable<string>> CopyReadmeFor(BuildContext context, PackageIdentity identity)
+    {
+        var reader = context.GetPackageReader(identity);
+        var readmeItem = reader.NuspecReader.GetReadme();
+        if (!String.IsNullOrWhiteSpace(readmeItem)) {
+            return await ExtractPackageItems(context, identity, [readmeItem]);
+        }
+
+        var description = GetDescription();
+
+        var readmePath = GetPackageDestination(context, identity).CombineWithFilePath("README.md");
+        await using FileStream stream = File.OpenWrite(readmePath.FullPath);
+        await using StreamWriter writer = new StreamWriter(stream);
+
+        await writer.WriteLineAsync($"# {GetTitle()}");
+        if (description is not null) {
+            await writer.WriteLineAsync();
+            await writer.WriteAsync(description);
+        }
+
+        return [readmePath.FullPath];
+
+        string GetTitle()
+        {
+            string title = reader.NuspecReader.GetTitle();
+            if (!string.IsNullOrWhiteSpace(title)) return title;
+
+            string id = reader.NuspecReader.GetId();
+            if (!string.IsNullOrWhiteSpace(id)) return id;
+
+            return identity.Id;
+        }
+
+        string? GetDescription()
+        {
+            string? description = reader.NuspecReader.GetDescription();
+            if (string.IsNullOrWhiteSpace(description)) return null;
+            return description;
+        }
+    }
+
+    private async Task<IEnumerable<string>> CopyLibItems(BuildContext context, PackageIdentity identity)
+    {
+        var items = (await FindLibItems(context, identity)).ToArray();
+        return await ExtractPackageItems(context, identity, items);
+    }
+
+    private async Task<IEnumerable<string>> CopyAllItems(BuildContext context, PackageIdentity identity)
+    {
+        var itemGroups = await Task.WhenAll(
+            CopyLicense(context, identity),
+            CopyReadmeFor(context, identity),
+            CopyLibItems(context, identity)
+        );
+        return itemGroups.SelectMany(x => x);
     }
 
     public override async Task RunAsync(BuildContext context)
     {
         var itemsForPackages = await Task.WhenAll(
-            context.PackageVersionsToBridge.Select(packageVersion => FindItemsFor(context, packageVersion.Identity))
+            context.PackageVersionsToBridge.Select(packageVersion => CopyAllItems(context, packageVersion.Identity))
         );
 
         context.RuntimeItemPaths = context.PackageVersionsToBridge
